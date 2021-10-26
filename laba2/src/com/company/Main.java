476a87f5f9052c364c9b4d2dbb14959eb5cd14ba
ch1llay <ch1llay@yandex.ru>
2021-10-26 17:33:38 +0300

    laba2

--- laba2/src/com/company/Main.java
+++ laba2/src/com/company/Main.java
@@ -1,3 +1,12 @@
+/*
+Вариант 8
+Проверить, надежно ли составлен пароль. Пароль считается надежным,
+если он состоит из 8 или более символов. Где символом может быть
+английская буква, цифра и знак подчеркивания. Пароль должен
+содержать хотя бы одну заглавную букву, одну маленькую букву и
+одну цифру.
+*/
+
 package com.company;
 
 import java.util.Scanner;
@@ -7,7 +16,7 @@ public class Main {
 
     public static void main(String[] args) {
         // (\d+|\w[a-z] | \w[A-z]+){8,}
-        String pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9_]).{8,}";
+        String pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*_).{8,}";
         Scanner in = new Scanner(System.in);
         boolean isCorrect = Pattern.matches(pattern, in.next());
         String result = isCorrect ? "correct" : "wrong";
